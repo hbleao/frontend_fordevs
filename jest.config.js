1a27a2135850825813dd6854a8f6639c1820baa6
hleao <hbragaleao@gmail.com>
2023-12-04 20:02:23 -0300

    chore: config jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  roots: ['<rootDir>/src'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+}
---
+++ src/test.spec.ts
@@ -0,0 +1,5 @@
+describe('test', () => {
+  it('a', () => {
+    console.log('testando...')
+  })
+})
