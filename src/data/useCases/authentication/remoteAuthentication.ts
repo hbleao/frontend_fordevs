6d6b0844c07d390dbb1eb5657dffebf40c3880fe
hleao <hbragaleao@gmail.com>
2023-12-05 14:15:31 -0300

    refact: add generic params to HttpPostClient

--- src/data/useCases/authentication/remoteAuthentication.ts
+++ src/data/useCases/authentication/remoteAuthentication.ts
@@ -2,13 +2,17 @@ import { HttpPostClient } from '@/data/protocols/http/httpPostClient'
 import { HttpStatusCode } from '@/data/protocols/http/httpResponse'
 import { InvalidCredentialsError } from '@/domain/erros/invalidCredentialsError'
 import { UnexpectedError } from '@/domain/erros/unexpectedError'
+import { AccountModel } from '@/domain/models/accoutModels'
 import { AuthenticationParams } from '@/domain/useCases/authentication'
 
 export class RemoteAuthentication {
   private url: string
-  private httpPostClient: HttpPostClient
+  private httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
 
-  constructor(url: string, httpPostClient: HttpPostClient) {
+  constructor(
+    url: string,
+    httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>,
+  ) {
     this.url = url
     this.httpPostClient = httpPostClient
   }
