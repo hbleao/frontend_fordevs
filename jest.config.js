3aaf11eaa88ee55575af761439c2f5e394d52935
hleao <hbragaleao@gmail.com>
2023-12-07 09:42:53 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,12 @@ module.exports = {
   roots: ['<rootDir>/src'],
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
   transform: {
     '.+\\.ts$': 'ts-jest',
   },
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy',
   },
 }
