b3be8dc0b1d588ee149a964d16b2a494f81b8761
hleao <hbragaleao@gmail.com>
2023-12-09 12:46:20 -0300

    test: ensure Login present valid state if password and email is valid

--- src/presentation/pages/login/index.spec.tsx
+++ src/presentation/pages/login/index.spec.tsx
@@ -76,6 +76,20 @@ describe('Login', () => {
     })
   })
 
+  it('should show valid email state if Validation success', () => {
+    const { validationSpy } = makeSut()
+    const email = screen.getByTestId('email')
+    const fakeEmail = faker.internet.email()
+    validationSpy.errorMessage = ''
+
+    fireEvent.input(email, { target: { value: fakeEmail } })
+
+    waitFor(() => {
+      const errorMessage = screen.queryByTestId('email-error')
+      expect(errorMessage).not.toBeInTheDocument()
+    })
+  })
+
   it('should show password error if validation fails', () => {
     const { validationSpy } = makeSut()
     const password = screen.getByTestId('password')
@@ -88,6 +102,21 @@ describe('Login', () => {
     waitFor(() => {
       const errorMessage = screen.getByTestId('password-error')
       expect(password.textContent).toBe(errorMessage)
+    })
+  })
+
+  it('should show valid password state if Validation success', () => {
+    const { validationSpy } = makeSut()
+    const password = screen.getByTestId('password')
+    const fakePassword = faker.internet.password()
+    const fakeErrorMessage = faker.lorem.words()
+    validationSpy.errorMessage = fakeErrorMessage
+
+    fireEvent.input(password, { target: { value: fakePassword } })
+
+    waitFor(() => {
+      const errorMessage = screen.queryByTestId('password-error')
+      expect(errorMessage).not.toBeInTheDocument()
     })
   })
 })
