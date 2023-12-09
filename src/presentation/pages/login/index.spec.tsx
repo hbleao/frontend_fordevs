af3a0edeadf4bc7bf8b46066abc16d61c31caf66
hleao <hbragaleao@gmail.com>
2023-12-09 11:58:18 -0300

    test: ensure login calls validation with correct email and password values

--- src/presentation/pages/login/index.spec.tsx
+++ src/presentation/pages/login/index.spec.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 import { Login } from '.'
+import { faker } from '@faker-js/faker'
+import { Validation } from '@/presentation/protocols'
+
+export class ValidationSpy implements Validation {
+  private errorMessage: string
+  public input: object
+
+  validate(input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
+}
 
 const makeSut = () => {
-  const sut = render(<Login />)
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Login validation={validationSpy} />)
 
   return {
     sut,
+    validationSpy,
   }
 }
 
@@ -26,5 +40,30 @@ describe('Login', () => {
 
     const password = screen.getByTestId('password-error')
     expect(password.textContent).toBe('Campo obrigatório')
+  })
+
+  it('should call validation with correct email value', () => {
+    const { validationSpy } = makeSut()
+    const email = screen.getByTestId('email')
+    const fakeEmail = faker.internet.email()
+
+    fireEvent.input(email, { target: { value: fakeEmail } })
+
+    expect(validationSpy.input).toEqual({
+      email: fakeEmail,
+    })
+  })
+
+  it('should call validation with correct password value', () => {
+    const { validationSpy } = makeSut()
+    const password = screen.getByTestId('password')
+
+    const fakePassword = faker.internet.password()
+
+    fireEvent.input(password, { target: { value: fakePassword } })
+
+    expect(validationSpy.input).toEqual({
+      password: fakePassword,
+    })
   })
 })
