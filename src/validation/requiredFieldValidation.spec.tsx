548ad73dec8e4f239ad0f9c69ddc16c55205ed83
hleao <hbragaleao@gmail.com>
2023-12-11 19:47:04 -0300

    test: create makeSut to requiredFieldValidation

--- src/validation/requiredFieldValidation.spec.tsx
+++ src/validation/requiredFieldValidation.spec.tsx
@@ -1,16 +1,25 @@
 import { faker } from '@faker-js/faker'
+
 import { RequiredFieldError } from './errors'
 import { RequiredFieldValidation } from './requiredFieldValidation'
 
+export const makeSut = () => {
+  const sut = new RequiredFieldValidation('email')
+
+  return {
+    sut,
+  }
+}
+
 describe('RequiredFieldValidation', () => {
   it('should return error if field empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const { sut } = makeSut()
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
   })
 
   it('should return falsy if field is not empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const { sut } = makeSut()
     const error = sut.validate(faker.internet.password())
     expect(error).toBeFalsy()
   })
