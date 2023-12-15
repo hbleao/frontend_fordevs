944309cbe525260f9beabac681b79930862cfe67
hleao <hbragaleao@gmail.com>
2023-12-15 15:30:47 -0300

    test: ensure CompareFieldsValidation returns error if validation fails

---
+++ src/validation/validators/compareFields/index.spec.ts
@@ -0,0 +1,22 @@
+import { faker } from '@faker-js/faker'
+
+import { InvalidFieldError, RequiredFieldError } from '@/validation/errors'
+import { CompareFieldsValidation } from '.'
+
+export const makeSut = (field: string, valueToCompare: string) => {
+  const sut = new CompareFieldsValidation(field, valueToCompare)
+
+  return {
+    sut,
+  }
+}
+
+describe('CompareFieldsValidation', () => {
+  it('should return error if compare is invalid', () => {
+    const value = faker.word.sample()
+    const valueToCompare = faker.word.sample()
+    const { sut } = makeSut('email', valueToCompare)
+    const error = sut.validate(value)
+    expect(error).toEqual(new InvalidFieldError())
+  })
+})
