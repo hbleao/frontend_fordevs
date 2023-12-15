0a876cc897a461f0b43be8cb922232ba92ce54f5
hleao <hbragaleao@gmail.com>
2023-12-15 15:33:02 -0300

    test: ensure CompareFieldsValidation returns falsy if validation success

--- src/validation/validators/compareFields/index.spec.ts
+++ src/validation/validators/compareFields/index.spec.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker'
 
-import { InvalidFieldError, RequiredFieldError } from '@/validation/errors'
+import { InvalidFieldError } from '@/validation/errors'
 import { CompareFieldsValidation } from '.'
 
 export const makeSut = (field: string, valueToCompare: string) => {
@@ -18,5 +18,12 @@ describe('CompareFieldsValidation', () => {
     const { sut } = makeSut('email', valueToCompare)
     const error = sut.validate(value)
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  it('should return falsy if compare is valid', () => {
+    const value = faker.word.sample()
+    const { sut } = makeSut('email', value)
+    const error = sut.validate(value)
+    expect(error).toBeFalsy()
   })
 })
