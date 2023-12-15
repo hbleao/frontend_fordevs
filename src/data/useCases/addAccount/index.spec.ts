ad44e3165e53371205285db5ca58cdd838561e83
hleao <hbragaleao@gmail.com>
2023-12-15 14:30:06 -0300

    test: ensure RemoteAddAccount calls HttpPostClient with correct url

---
+++ src/data/useCases/addAccount/index.spec.ts
@@ -0,0 +1,36 @@
+import { faker } from '@faker-js/faker'
+
+import { RemoteAddAccount } from '.'
+
+import { AddAccountParams } from '@/domain/useCases'
+import { AccountModel } from '@/domain/models'
+import { HttpPostClientSpy } from '@/data/test'
+import { mockAddAccountParams } from '@/domain/test'
+
+type SutTypes = {
+  sut: RemoteAddAccount
+  httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>
+}
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy<
+    AddAccountParams,
+    AccountModel
+  >()
+  const sut = new RemoteAddAccount(url, httpPostClientSpy)
+
+  return {
+    sut,
+    httpPostClientSpy,
+  }
+}
+
+describe('RemoteAddAccount', () => {
+  it('should call HttpPostClient with correct url', async () => {
+    const url = faker.internet.url()
+    const { sut, httpPostClientSpy } = makeSut(url)
+    await sut.add(mockAddAccountParams())
+
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
